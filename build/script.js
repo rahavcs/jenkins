4a71d8b2c75924d5ed4aaafeccf686f4dbdd1bab
rahavcs <149079223+rahavcs@users.noreply.github.com>
2025-02-05 23:15:56 +0530

    Add files via upload

---
+++ build/script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'Hello, World! You just clicked the button!';
+    messageElement.style.color = 'blue';
+}
