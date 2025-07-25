8de249d72951c0b728871cb94ef43e7866625e86
rahavcs <149079223+rahavcs@users.noreply.github.com>
2025-07-25 12:48:16 +0530

    Delete build directory

--- build/script.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = 'Hello, World! You just clicked the button!';
-    messageElement.style.color = 'blue';
-}
